fd2d72cf416ed11e34b1b364dbf1686e1fc42870
Christian Studer <studer6u@fst-ats-319-l07.fst.site.univ-lorraine.fr>
2016-05-11 13:03:03 +0200

    Le commencement de ton oncle

---
+++ src/spam/FiltreAntiSpam.java
@@ -0,0 +1,13 @@
+package spam;
+
+import java.io.IOException;
+
+public class FiltreAntiSpam {
+	private static String[] dico;
+	private static boolean[] message;
+	
+	public static void charger_dictionnaire(String fichier)throws IOException{
+		
+	}
+	
+}
