8a994783fb07686e776dc94f99d73fc7cebfbb83
Christian Studer <studer6u@fst-ats-319-l07.fst.site.univ-lorraine.fr>
2016-05-11 15:55:12 +0200

    chargement dico correct + début lire_message à compléter

--- src/spam/FiltreAntiSpam.java
+++ src/spam/FiltreAntiSpam.java
@@ -3,9 +3,10 @@ package spam;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FiltreAntiSpam {
-	private static String[] dico = new String[1000];
+	private static ArrayList<String> dico = new ArrayList<String>();
 	private static boolean[] message;
 	
 	public static void main(String[] args) throws IOException{
@@ -13,15 +14,26 @@ public class FiltreAntiSpam {
 	}
 	
 	public static void charger_dictionnaire(String fichier)throws IOException{
-		int i=0;
 		try {
 			BufferedReader bfr = new BufferedReader(new FileReader(fichier));
 			String ligne;
 			while((ligne=bfr.readLine()) != null){
-				dico[i]=ligne;
-				i++;
+				if (ligne.length()>=3)
+					dico.add(ligne);
 			}
 			bfr.close();
+		}
+		catch (IOException e){ 
+            System.out.println(e.getMessage()); 
+            System.exit(1);
+		}
+	}
+	
+	public static void lire_message(String fichier) throws IOException{
+		message = new boolean[dico.size()];
+		try {
+			BufferedReader bfr = new BufferedReader(new FileReader(fichier));
+			String mot;
 		}
 		catch (IOException e){ 
             System.out.println(e.getMessage()); 
