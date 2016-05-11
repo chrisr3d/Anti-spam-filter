92d5d3a9b455da085b245f04ac5844a3fa94f94a
Christian Studer <studer6u@fst-ats-319-l07.fst.site.univ-lorraine.fr>
2016-05-11 14:29:48 +0200

    CHARGEMENT DE DICTIONNAIRE SWAG

--- src/spam/FiltreAntiSpam.java
+++ src/spam/FiltreAntiSpam.java
@@ -1,13 +1,32 @@
 package spam;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FiltreAntiSpam {
-	private static String[] dico;
+	private static String[] dico = new String[1000];
 	private static boolean[] message;
 	
+	public static void main(String[] args) throws IOException{
+		charger_dictionnaire("dictionnaire1000en.txt");
+	}
+	
 	public static void charger_dictionnaire(String fichier)throws IOException{
-		
+		int i=0;
+		try {
+			BufferedReader bfr = new BufferedReader(new FileReader(fichier));
+			String ligne;
+			while((ligne=bfr.readLine()) != null){
+				dico[i]=ligne;
+				i++;
+			}
+			bfr.close();
+		}
+		catch (IOException e){ 
+            System.out.println(e.getMessage()); 
+            System.exit(1);
+		}
 	}
 	
 }
