f444fa5b2f316e0e3e55ffaf7e83662c7b3e2e2f
Misternutz <pierre.leriche94@gmail.com>
2016-05-13 12:15:56 +0200

    Apprentissage Ham/Spam

--- src/spam/FiltreAntiSpam.java
+++ src/spam/FiltreAntiSpam.java
@@ -9,11 +9,88 @@ import java.util.Scanner;
 public class FiltreAntiSpam {
 	private static ArrayList<String> dico = new ArrayList<String>();
 	private static boolean[] message;
+	private static boolean[][] ensembleMessageHam;
+	private static boolean[][] ensembleMessageSpam;
+	private static double[] probaHam;
+	private static double[] probaSpam;
+	
+	
+	
 	
 	public static void main(String[] args) throws IOException{
 		charger_dictionnaire("dictionnaire1000en.txt");
-		lire_message("baseapp/spam/test.txt");
+		lire_message("baseapp/spam/0.txt");
+		System.out.println("Entrer un nombre de Ham");
+		Scanner sc = new Scanner(System.in);
+		/*
+		int nbSpam = sc.nextInt();
+		appprentissageSpam(nbSpam);
+		
+		
+		for(int j = 0; j<probaSpam.length;j++){
+			System.out.println(probaSpam[j]);
+		}
+		*/
+		int nbHam = sc.nextInt();
+		appprentissageHam(nbHam);
+		
+		
+		for(int j = 0; j<probaHam.length;j++){
+			System.out.println(probaHam[j]);
+		}
+		
+		
+		
+		
 	}
+	
+	public static void appprentissageHam(int nbFichier) throws IOException{
+		ensembleMessageHam = new boolean[nbFichier][dico.size()];
+		probaHam = new double[dico.size()];
+		for(int i =0; i<nbFichier; i++){
+			
+			lire_message("baseapp/ham/"+i+".txt");
+			ensembleMessageHam[i] = message;
+			}
+		
+		for(int j = 0; j<dico.size();j++){
+			
+			for(int k = 0; k<nbFichier;k++){
+				
+				if( ensembleMessageHam[k][j] == true){
+					probaHam[j]++;
+				}
+			}
+			
+			probaHam[j] = (probaHam[j]/nbFichier);
+		}
+		}
+		
+	
+	public static void appprentissageSpam(int nbFichier) throws IOException{
+		ensembleMessageSpam = new boolean[nbFichier][dico.size()];
+		probaSpam = new double[dico.size()];
+		for(int i =0; i<nbFichier; i++){
+			
+			lire_message("baseapp/spam/"+i+".txt");
+			ensembleMessageSpam[i] = message;
+			}
+		
+		for(int j = 0; j<dico.size();j++){
+			
+			for(int k = 0; k<nbFichier;k++){
+				
+				if( ensembleMessageSpam[k][j] == true){
+					probaSpam[j]++;
+				}
+			}
+			
+			probaSpam[j] = (probaSpam[j]/nbFichier);
+			
+		}
+			
+		}
+		
 	
 	public static void charger_dictionnaire(String fichier)throws IOException{
 		try {
@@ -54,7 +131,7 @@ public class FiltreAntiSpam {
 		catch (IOException e){ 
             System.out.println(e.getMessage()); 
             System.exit(1);
-            //Christian est un penis
+            
 		}
 	}
 	
